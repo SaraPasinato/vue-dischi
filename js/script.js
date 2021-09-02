a2f13b8e1871cbdf55d18a1a8598ac4effb79d54
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-02 16:30:45 +0200

    add albums api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+/** 
+ * ! Descrizione:
+* Attraverso una chiamata ajax all’API di boolean:
+* https://flynn.boolean.careers/exercises/api/array/music
+* avremo a disposizione una decina di dischi musicali.
+* Utilizzando vue, stampiamo a schermo una card per ogni album.
+* in allegato la cartellina col template fatto insieme e uno screen
+* del risultato richiesto
+* !BONUS:
+* Ordinare i dischi per anno di uscita.
+* Creare una select con tutti i generi dei dischi. In base a cosa
+* scegliamo nella select, vedremo i corrispondenti cd. 
+*/
 console.log("HERE",Vue);
 
 Vue.config.devtools=true;
@@ -7,6 +20,16 @@ Vue.config.devtools=true;
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
-    }
+     albums:[],
+    },
+    methods:{
+      getDiscs(){
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res)=>{
+        this.albums=res.data.response;
+        })
+      }
+    },
+    created(){
+     this.getDiscs();
+    },
   })
