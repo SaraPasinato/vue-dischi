3b019423055bd7a4555b45efc393e7f019ae2fe8
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-02 16:53:09 +0200

    add filterByYear

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 ** Utilizzando vue, stampiamo a schermo una card per ogni album.
 * 
 * !BONUS:
-* TODO 1)Ordinare i dischi per anno di uscita.
+** TODO 1)Ordinare i dischi per anno di uscita.
 * TODO 2)Creare una select con tutti i generi dei dischi. In base a cosa
 * TODO       scegliamo nella select, vedremo i corrispondenti cd. 
 */
@@ -25,11 +25,17 @@ var app = new Vue({
       getDiscs(){
         axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res)=>{
         this.albums=res.data.response;
+        this.filterByYear();
         })
-      }
+      },
+      filterByYear(){
+        return (this.albums.sort((a,b)=> parseInt(a.year) - parseInt(b.year)));
+        },
     },
     created(){
      this.getDiscs();
     },
-    
+    updated:{
+     
+    }
   })
